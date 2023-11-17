cc991728e9fc45ea60c16681db1dd4eefe2f4562
rcoletti116 <coletti.robert@gmail.com>
2023-11-16 20:19:33 -0500

    Update InfinitudeSensor.js
    revert changes

--- src/InfinitudeSensor.js
+++ src/InfinitudeSensor.js
@@ -10,7 +10,7 @@ module.exports = class InfinitudeSensor {
     Service = service;
     Characteristic = characteristic;
 
-    this.initialize = platformAccessory.getService(Service.TemperatureSensor);
+    this.temperatureService = platformAccessory.getService(Service.TemperatureSensor);
     this.bindInformation(platformAccessory.getService(Service.AccessoryInformation));
 
     // Update the temperature every 2 minutes (120000 milliseconds)
@@ -29,22 +29,11 @@ module.exports = class InfinitudeSensor {
     }
   }
 
-    initialize(service) {
-    service.getCharacteristic(Characteristic.CurrentTemperature).on(
-      'get',
-      function (callback) {
-        this.getCurrentOutdoorTemperature().then(function (currentTemperature) {
-          callback(null, currentTemperature);
-        });
-      }.bind(this)
-    );
-  }
-
   updateTemperature() {
     this.getCurrentOutdoorTemperature()
       .then(currentTemperature => {
         // Update the TemperatureSensor characteristic with the new temperature value
-        this.initialize.updateCharacteristic(Characteristic.CurrentTemperature, currentTemperature);
+        this.temperatureService.updateCharacteristic(Characteristic.CurrentTemperature, currentTemperature);
         this.log.info(`Outdoor temperature updated to: ${currentTemperature}°C`);
       })
       .catch(error => {
